a27eeff79ffb5d9bafa388cdb259a923d8d40d93
Donald Chanthirath <100371989+dchanthirath@users.noreply.github.com>
2024-02-03 13:20:09 -0600

    updated cmake folder, lab files, project files, and started report

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CS-201
+This repository is my history of projects and assignments for this class
--- project/phase1/CircularDynamicArray.cpp
+++ project/phase1/CircularDynamicArray.cpp
@@ -2,109 +2,154 @@
 // Created by Donald on 1/29/2024.
 //
 
-#include "CircularDynamicArray.h"
+template <typename elmtype>
+class CircularDynamicArray {
+public:
+    CircularDynamicArray();
+    explicit CircularDynamicArray(int s);
+    ~CircularDynamicArray();
 
-CircularDynamicArray::CircularDynamicArray() {
+    elmtype& operator[](int i);
+    void addEnd(elmtype v);
+    void addFront(elmtype v);
+    void delEnd();
+    void delFront();
+
+    int length();
+    int capacity();
+    void clear();
+
+    elmtype QSelection(int k);
+    void sort();
+    int linearSearch(elmtype e);
+    int binSearch(elmtype e);
+
+private:
+    int size, capacitySize;
+    // int front, back;
+    int *array;
+
+};
+
+template <typename elmtype>
+CircularDynamicArray<elmtype>::CircularDynamicArray() {
     // Default Constructor.
     // The array should be of capacity 2 and size 0
-    capacitySize = 2;
+    array = new elmtype[capacitySize = 2];
     size = 0;
-    a = new int[capacitySize];
 
 }
 
-CircularDynamicArray::CircularDynamicArray(int s) {
+template <typename elmtype>
+CircularDynamicArray<elmtype>::CircularDynamicArray(int s) {
     // For this constructor the array should be of capacity and size s
-    capacitySize = s;
+    array = new elmtype[capacitySize = s];
     size = s;
-    a = new int[capacitySize];
 
 }
 
-CircularDynamicArray::~CircularDynamicArray() {
+template <typename elmtype>
+CircularDynamicArray<elmtype>::~CircularDynamicArray() {
     // destructor for the class
-    delete[] a;
+    delete[] array;
 
 }
 
-elmtype& CircularDynamicArray::operator[](int i) {
+template <typename elmtype>
+elmtype& CircularDynamicArray<elmtype>::operator[](int i) {
     // Traditional [] operator.
     // Should print a message if i is out of bounds and
     // return a reference to value of type elmtype stored in the class for this purpose
 
 }
-void CircularDynamicArray::addEnd(elmtype v) {
+
+template <typename elmtype>
+void CircularDynamicArray<elmtype>::addEnd(elmtype v) {
     // increases the size of the array by 1 and stores v at the end of the array.
     // Should double the capacity when the new element doesn't fit.
+    if (size == capacitySize) capacitySize *= 2;
+    size++;
+    // TODO: implement the addEnd function
 
 }
-void CircularDynamicArray::addFront(elmtype v) {
+
+template <typename elmtype>
+void CircularDynamicArray<elmtype>::addFront(elmtype v) {
     // increases the size of the array by 1 and stores v at the beginning of the array.
     // Should double the capacity when the new element doesn't fit.
     // The new element should be the item returned at index 0.
+    if (size == capacitySize) capacitySize *= 2;
+    size++;
 
+    // TODO:: implement the addfront functionS
 }
 
-void CircularDynamicArray::delEnd() {
+template <typename elmtype>
+void CircularDynamicArray<elmtype>::delEnd() {
     // reduces the size of the array by 1 at the end.
     // Should shrink the capacity when only 25% of the array is in use after the delete.
+    if (size <= (capacitySize / 4)) capacitySize /= 2;
     size--;
-    if (capacitySize >= (size / 4))
-    {
-        size /= 2;
-        //FIXME: make a new array and insert old values into new array
-    }
 
+    // TODO: make a new array and insert old values into new array
 }
 
-void CircularDynamicArray::delFront() {
+template <typename elmtype>
+void CircularDynamicArray<elmtype>::delFront() {
     // reduces the size of the array by 1 at the beginning of the array.
     // Should shrink the capacity when only 25% of the array is in use after the delete.
+    if (size <= (capacitySize / 4)) capacitySize /= 2;
     size--;
-    if (capacitySize >= (size / 4))
-    {
-        size /= 2;
 
-        // FIXME: make new array and insert old values into that
-    }
-
+    // TODO: actually implement this lmao
 }
 
-int CircularDynamicArray::length() {
+template <typename elmtype>
+int CircularDynamicArray<elmtype>::length() {
     // returns the size of the array
     return size;
 
 }
 
-int CircularDynamicArray::capacity() {
+template <typename elmtype>
+int CircularDynamicArray<elmtype>::capacity() {
     // returns the capacity of the array
     return capacitySize;
 
 }
-void CircularDynamicArray::clear() {
+
+template <typename elmtype>
+void CircularDynamicArray<elmtype>::clear() {
     // Frees any space currently used and starts over with an array of capacity 2 and size 0
 
+    delete[] array;
+    array = new int[capacitySize = 2];
+    size = 0;
 }
 
-Elmtype CircularDynamicArray::QSelection(int k) {
+template <typename elmtype>
+elmtype CircularDynamicArray<elmtype>::QSelection(int k) {
     // returns the kth smallest element in the array using the quickselect algorithm.
     // This method should choose a pivot element at random.
 
 }
 
-void CircularDynamicArray::sort() {
+template <typename elmtype>
+void CircularDynamicArray<elmtype>::sort() {
     // Sorts the values in the array using a comparison based O(N lg N) algorithm.
     // The sort must be stable.
 
 }
 
-int CircularDynamicArray::linearSearch(elmtype e) {
+template <typename elmtype>
+int CircularDynamicArray<elmtype>::linearSearch(elmtype e) {
     // Performs a linear search of the array looking for the item e.
     // Returns the index of the item if found or -1 otherwise.
 
 }
 
-int CircularDynamicArray::binSearch(elmtype e) {
+template <typename elmtype>
+int CircularDynamicArray<elmtype>::binSearch(elmtype e) {
     // Performs a binary search of the array looking for the item e.
     // Returns the index of the item if found or -1 otherwise.
     // This method assumes that the array is in increasing order, but there is no guarantee that the sort method has been called.
